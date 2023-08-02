a52804eb40b5ba34d0067e2e0eff1f5ab46b11ec
Arcel <git@github.com:Arcellv/SimpleEcommerce_website.git>
2023-08-02 13:51:38 +0300

    update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
 
 import { Container, Row, Col } from 'reactstrap'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import '../../styles/Header.css'
+
+const navLinks = [
+  {
+    path: "/home",
+    display: "Home",
+  },
+  {
+    path: "/about",
+    display: "About",
+  },
+  {
+    path: "/cars",
+    display: "Cars",
+  },
+  {
+    path: "/blogs",
+    display: "Blogs",
+  },
+  {
+    path: "/contact",
+    display: "Contact",
+  },
+];
 
 const Header = () => {
   return (
@@ -21,16 +44,99 @@ const Header = () => {
             </Col>
 
             <Col lg="6" md="6" sm="6">
-              <div className="header_top_right">
-                <Link to="#"> 
+              <div className="header_top_right d-flex align-items-center justify-content-end gap-3">
+                <Link to="#" className="d-flex align-items-center gap-1">
                   <i class="ri-login-circle-line"></i>Log in
                 </Link>
-                <Link to="#">
+                <Link to="#" className="d-flex align-items-center gap-1">
                   <i class="ri-user-line"></i>Sign up
                 </Link>
               </div>
             </Col>
           </Row>
+        </Container>
+      </div>
+
+      {/*========================= Header Middle ======================= */}
+      <div className="header_middle">
+        <Container>
+          <Row>
+            <Col lg="4" md="3" sm="4">
+              <div className="logo">
+                <h1>
+                  <Link to="/home" className="d-flex align-items-center gap-2">
+                    <i class="ri-roadster-fill"></i>
+                    <span>
+                      Rent Car <br /> Service
+                    </span>
+                  </Link>
+                </h1>
+              </div>
+            </Col>
+
+            <Col lg="3" md="3" sm="4">
+              <div className="header_location d-flex align-items-center gap-2">
+                <span>
+                  <i class="ri-earth-line"></i>
+                </span>
+                <div className="header_location-content">
+                  <h4>Northern Cyprus</h4>
+                  <h6>Nicosia, Northern Cyprus</h6>
+                </div>
+              </div>
+            </Col>
+
+            <Col lg="3" md="3" sm="4">
+              <div className="header_location d-flex align-items-center gap-2">
+                <span>
+                  <i class="ri-time-line"></i>
+                </span>
+                <div className="header_location-content">
+                  <h4>Monday to Friday</h4>
+                  <h6>9am - 4pm</h6>
+                </div>
+              </div>
+            </Col>
+
+            <Col lg="2" md="3" sm="0" className="text-end">
+              <button className="header_btn btn">
+                <Link to="/contact">
+                  <i class="ri-phone-line"></i> Call Request
+                </Link>
+              </button>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+
+      {/*======================== Main Navigation =====================*/}
+      <div className="main_navbar">
+        <Container>
+          <div className="navigation_wrapper d-flex align-items-center justify-content-between">
+            <span className="mobile_menu">
+            <i class="ri-menu-line"></i>
+            </span>
+
+            <div className="navigation">
+            <div className="menu">
+            {
+              navLinks.map((item, index)=>(
+                <NavLink to={item.path}  className="nav_item" key={index} > {item.display}
+                </NavLink>
+              ))
+            }
+            </div>
+            </div>
+            
+            <div className="nav_right">
+              <div className="search_box">
+              <input type="text" placeholder="Search" />
+              <span>
+                <i class="ri-search-line"></i>
+              </span>
+              </div>
+            </div>
+          </div>
         </Container>
       </div>
     </header>
