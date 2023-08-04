ab8ec8e232427e7ab92814aa50e7d9f6ae6e2d51
Arcel <git@github.com:Arcellv/SimpleEcommerce_website.git>
2023-08-04 04:06:24 +0300

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,8 @@ import React from 'react'
 import HeroSlider from '../components/UI/HeroSlider'
 import Helmet from '../components/helmet/Helmet'
 
+import { Container, Row, Col } from 'reactstrap'
+
 const Home = () => {
   return <Helmet title="Home">
     <section className="p-0 hero_slider-section">
