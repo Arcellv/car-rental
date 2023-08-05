0381feef78b298cd1caf6d21b61b7fed7af8db64
Arcel <git@github.com:Arcellv/SimpleEcommerce_website.git>
2023-08-05 16:07:14 +0300

    update

--- src/components/UI/CarForm.js
+++ src/components/UI/CarForm.js
@@ -1,10 +1,46 @@
 import React from 'react'
 import '../../styles/CarForm.css'
+import "../../styles/CarForm.css"
+
+import { Form, FormGroup  } from 'reactstrap'
+
 
 const CarForm = () => {
   return (
-    <div>CarForm</div>
-  )
+    <Form className="form">
+      <div className="d-flex aligns-items-center justify-content-between flex-wrap">
+        <FormGroup className="form_group">
+          <input type="text" placeholder=" From address" required />
+        </FormGroup>
+
+        <FormGroup className="form_group">
+          <input type="text" placeholder=" to address" required />
+        </FormGroup>
+
+        <FormGroup className="form_group">
+          <input type="date" placeholder="Journey date" required />
+        </FormGroup>
+
+        <FormGroup className="form_group">
+          <input  className="Journey_time" type="time" placeholder="Journey time" required />
+          <span>
+            <i class="ri-time-line"></i>
+          </span>
+        </FormGroup>
+
+        <FormGroup className="select_group">
+          <select>
+            <option value="ac"> AC Car</option>
+            <option value="non-ac"> None-AC Car</option>
+          </select>
+        </FormGroup>
+
+        <FormGroup className="form_group">
+          <button className="btn car_btn">Find car</button>
+        </FormGroup>
+      </div>
+    </Form>
+  );
 }
 
 export default CarForm
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import HeroSlider from '../components/UI/HeroSlider'
 import Helmet from '../components/helmet/Helmet'
+import CarForm from '../components/UI/CarForm'
 
 import { Container, Row, Col } from 'reactstrap'
 
@@ -9,6 +10,22 @@ const Home = () => {
   return <Helmet title="Home">
     <section className="p-0 hero_slider-section">
       <HeroSlider/>
+
+      <div className="hero_form">
+        <Container>
+          <Row className="form_row">
+            <Col lg="4" md="4">
+              <div className="cars_left">
+                <h2>Find your best car here</h2>
+              </div>
+            </Col>
+
+            <Col lg="8" md="8" sm="12">
+              <CarForm />
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </section>
   </Helmet>
 }
