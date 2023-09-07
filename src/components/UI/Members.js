02bfa5375aad96fd64912a4ba0fbb509b6771316
Arcel <lomendjaa@gmail.com>
2023-09-07 05:11:26 +0300

    update

---
+++ src/components/UI/Members.js
@@ -0,0 +1,87 @@
+import React from "react";
+import "../../styles/Members.css";
+import { Col } from "reactstrap";
+import { Link } from "react-router-dom";
+
+import ava05 from "../../assets/all-images/ava05.webp";
+import ava06 from " ../../assets/all-images/ava06.jpg";
+import ava07 from " ../../assets/all-images/ava07.avif";
+import ava08 from " ../../assets/all-images/ava08.jpg";
+
+const Member = [
+  {
+    name: "John Doe",
+    experience: "5 years of experience",
+    fbUrl: "#",
+    instUrl: "#",
+    twitUrl: "#",
+    linkedinUrl: "#",
+    imgUrl: ava05,
+  },
+
+  {
+    name: "David Lisa",
+    experience: "5 years of experience",
+    fbUrl: "#",
+    instUrl: "#",
+    twitUrl: "#",
+    linkedinUrl: "#",
+    imgUrl: ava06,
+  },
+
+  {
+    name: "Hilton King",
+    experience: "5 years of experience",
+    fbUrl: "#",
+    instUrl: "#",
+    twitUrl: "#",
+    linkedinUrl: "#",
+    imgUrl: ava07,
+  },
+
+  {
+    name: "Mahamud Doe",
+    experience: "5 years of experience",
+    fbUrl: "#",
+    instUrl: "#",
+    twitUrl: "#",
+    linkedinUrl: "#",
+    imgUrl: ava08,
+  },
+];
+
+const Members = () => {
+  return (
+    <>
+      {Member.map((item, index) => (
+        <Col lg="3" md="3" sm="4" xs="6" key={index} className="mb-4">
+          <div className="single_member">
+            <div className="single_member-img">
+              <img src={item.imgUrl} alt=""  className="w-100"/>
+
+              <div className="single_member-social">
+                <Link to={item.fbUrl}>
+                  <i class="ri-facebook-line"></i>
+                </Link>
+                <Link to={item.twitUrl}>
+                  <i class="ri-twitter-line"></i>
+                </Link>
+                <Link to={item.linkedinUrl}>
+                  <i class="ri-linkedin-line"></i>
+                </Link>
+                <Link to={item.instUrl}>
+                  <i class="ri-instagram-line"></i>
+                </Link>
+              </div>
+            </div>
+
+            <h6 className="text-center mb-0 mt-3">{item.name}</h6>
+            <p className="description_section">{item.experience}</p>
+          </div>
+        </Col>
+      ))}
+    </>
+  );
+};
+
+export default Members;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,73 @@
-
-
 import React from 'react'
+import Helmet from '../components/helmet/Helmet'
+import CommonSection from '../components/UI/CommonSection'
+import AboutSection from '../components/UI/AboutSection'
+import { Container, Row, Col } from 'reactstrap'
+import DriverSection from '../components/UI/DriverSection'
+import DriveImg from '../assets/all-images/drive.webp'
+import Members from '../components/UI/Members'
 
 const About = () => {
   return (
-    <div>About</div>
-  )
+    <Helmet title="About">
+      <CommonSection title="About Us" />
+      <AboutSection />
+
+      <section className="page_section">
+        <Container>
+          <Row>
+            <Col lg="6" md="6" sm="12">
+              <div className="page_img">
+                <img src={DriveImg} alt="" className="w-100 rounded-3" />
+              </div>
+            </Col>
+
+            <Col lg="6" md="6" sm="12">
+              <div className="page_content">
+                <h2 className="section_title">
+                  We Commit Ourself to Provide Safe Solution for your Best
+                  Interest
+                </h2>
+                <p className="description_section">
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Veniam consectetur beatae dolorum rerum architecto suscipit.
+                  Doloribus ullam tempora reprehenderit cum?
+                </p>
+                <p className="description_section">
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Veniam consectetur beatae dolorum rerum architecto suscipit.
+                  Doloribus ullam tempora reprehenderit cum?
+                </p>
+
+                <div className="d-flex align-items-center gap-3 mt-4">
+                  <span><i class="ri-phone-line"></i></span>
+                </div>
+
+                <div>
+                  <h6 className="subtitle_section">Need Any Help</h6>
+                  <h4>+905338840966</h4>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <DriverSection />
+
+      <section >
+        <Container>
+          <Row>
+            <Col lg="12" className="mb-5 text-center">
+              <h6 className="subtitle_section">Experts</h6>
+              <h2 className="title_section"> Our Members</h2>
+            </Col>
+            <Members/>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
 }
 
 export default About
