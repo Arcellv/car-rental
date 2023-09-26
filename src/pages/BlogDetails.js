14c7fa1d4418bbede547bcba72c5fe22d841cba6
Arcel <lomendjaa@gmail.com>
2023-09-26 23:12:16 +0300

    update

--- src/pages/BlogDetails.js
+++ src/pages/BlogDetails.js
@@ -1,11 +1,48 @@
+import React, { useEffect } from 'react'
+import { Container, Row, Col } from 'reactstrap'
 
-
-import React from 'react'
+import { useParams } from 'react-router-dom'
+import blogData from '../assets/data/dataBlog'
+import Helmet from '../components/helmet/Helmet'
 
 const BlogDetails = () => {
+  
+  const {slug} =  useParams()
+  const blog = blogData.find(blog => blog.title === slug )
+
+  useEffect(() => {
+    window.scrollTo(0,0)
+  },[blog])
+
   return (
-    <div>BlogDetails</div>
-  )
+    <Helmet title={blog.title}>
+      <Container>
+        <Row>
+          <Col lg="8" md="8">
+            <div className="blog_details">
+              <img src={blog.imgUrl} alt="" className="w-100" />
+              <h2 className="section_title">{blog.time}</h2>
+
+              <div className="blog_publisher d-flex align-items-center gap-4 mb-4">
+                <span className="blog_author">
+                  <i class="ri-user-line"></i> {blog.author}
+                </span>
+
+                <span className="d-flex align-items-center gap-1 description_section">
+                  <i class="ri-calendar-line"></i> {blog.date}
+                </span>
+
+                <span className="d-flex align-items-center gap-1 description_section">
+                  <i class="ri-time-line"></i> {blog.time}
+                </span>
+              </div>
+              <p className="description_section">{blog.description}</p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </Helmet>
+  );
 }
 
 export default BlogDetails
--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -1,11 +1,22 @@
-
-
 import React from 'react'
+import { Container, Row } from 'reactstrap'
+import Helmet from '../components/helmet/Helmet'
+import CommonSection from '../components/UI/CommonSection'
+import BlogList from '../components/UI/BlogList'
 
 const Blogs = () => {
   return (
-    <div>Blogs</div>
-  )
+    <Helmet title="Blogs">
+      <CommonSection title="Blogs" />
+      <section>
+        <Container>
+          <Row>
+            <BlogList />
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
 }
 
 export default Blogs
