d60309fb663edf823602a782f4c378aefd8cc3c5
Arcel <lomendjaa@gmail.com>
2023-09-26 12:12:16 +0300

    update

--- src/components/UI/Booking.js
+++ src/components/UI/Booking.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import '../../styles/Booking.css'
+import { Form, FormGroup } from 'reactstrap'
 
 const Booking = () => {
   return (
-    <div>Booking</div>
-  )
+    <Form>
+      <FormGroup className="booking_form d-inline-block me-4 mb-4">
+        <input type="text" placeholder="First-Name" />
+      </FormGroup>
+
+      <FormGroup className="booking_form d-inline-block ms-1 mb-4">
+        <input type="text" placeholder="First-Name" />
+      </FormGroup>
+
+      <FormGroup className="booking_form d-inline-block me-4 mb-4">
+        <input type="Email" placeholder="Email" />
+      </FormGroup>
+
+      <FormGroup className="booking_form d-inline-block ms-1 mb-4">
+        <input type="number" placeholder="phone number" />
+      </FormGroup>
+      <FormGroup className="booking_form d-inline-block me-4 mb-4">
+        <input type="text" placeholder=" From Address" />
+      </FormGroup>
+
+      <FormGroup className="booking_form d-inline-block ms-1 mb-4">
+        <input type="text" placeholder="To Address" />
+      </FormGroup>
+    </Form>
+  );
 }
 
 export default Booking
